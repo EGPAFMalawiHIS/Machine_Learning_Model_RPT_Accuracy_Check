ee20bc59a2b0fade128ca54953feabb5fbe29822
cmaere <charlie@rombot.com>
2020-08-26 18:12:53 +0200

    data extraction

--- common/helper.py
+++ common/helper.py
@@ -1,5 +1,6 @@
 import json
 import glob
+import os
 class Helper:
 	def loadJson(self,file):
 		data = json.load(file)
@@ -7,3 +8,13 @@ class Helper:
 	def collectFiles(self,data_folder,ext):
 		chafiles = glob.glob(data_folder+'/*'+ext)
 		return chafiles
+	def chaStrip(self,data):
+		data = str(data)
+		data = data.strip()
+		stripped = data.rstrip()
+		return stripped
+	def jsonToDict(self, data):
+		return json.loads(data)
+	def checkIfFileExists(self,file):
+		return os.path.isdir(file)
+
--- preprocessing/preprocessing_part1.py
+++ preprocessing/preprocessing_part1.py
@@ -6,12 +6,43 @@ from common.helper import *
 
 class IndicatorMapper:
 	def __init__(self):
-		self.data_folder = './data'
+		self.data_folder = './rawdata'
+		self.dataset_folder = './dataset'
 		self.helper = Helper()
 
 	def getSite(self,data):
-		site = []
-		site.append({sitecode:data['sitecode'],sitename:data['sitename']})
+		sitecode = self.helper.chaStrip(data['sitecode'])
+		sitename = self.helper.chaStrip(data['sitename'])
+		site = {'sitecode':sitecode,'sitename':sitename}
+		return site
+	def generatePath(self,site):
+		path = self.dataset_folder + site.lower() + '.h5'
+		return path
+	def pepfar_disagg(self,data):
+		return 0
+
+	def extractData(self,data):
+		#check report name
+		report_data = []
+		report_name = data['report_name']
+		if report_name == 'PEPFAR Disaggregated Report':
+			report_data = self.pepfar_disagg(data)
+		return report_data
+
+
+	def mapper(self,data):
+		site = self.getSite(data)
+		#check is this site exit in our internal db
+		file_path = self.generatePath(site['sitecode'])
+		if self.helper.checkIfFileExists(file_path):
+			extracted_data = self.extractData(data)
+		else:
+			extracted_data = self.extractData(data)
+
+
+		print('EXIST:',self.helper.checkIfFileExists(file_path))
+		sys.exit()
+
 
 	def main(self):
 		chafiles = self.helper.collectFiles(self.data_folder,'.json')
@@ -19,8 +50,5 @@ class IndicatorMapper:
 			files_json = file
 			with open(files_json) as jsonfile:
 				data = self.helper.loadJson(jsonfile)
-				print('Data:',data)
-				sys.exit()
-				for category in categories:
-					print(category)
-					sys.exit()
+				mapped_data = self.mapper(data)
+
